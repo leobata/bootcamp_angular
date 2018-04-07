dbe7251f517f4191c1ba75268845aea7682850fe
Leonardo de Oliveira <leonardo.battagin@venturus.org.br>
2018-04-07 11:32:25 -0300

    Creating items list

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  private listItems: Array<any>;
+
+  constructor() {
+    this.listItems = [{
+      name: 'Bread',
+      disabled: false
+    },
+    {
+      name: 'Butter',
+      disabled: false
+    },
+    {
+      name: 'Coffee',
+      disabled: false
+    },
+    {
+      name: 'Cookies',
+      disabled: true
+    }];
+  }
+
+  ngOnInit() {
+
+  }
+
+}
